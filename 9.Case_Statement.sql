e66067b5d48a57fc1034c92b1bd3f1a4225fae6e
Abbas Rabbani <mail.abbas.rabbani@gmail.com>
2026-03-21 10:02:37 +0100

    Example for case statement for increase in salary

--- 9.Case_Statement.sql
+++ 9.Case_Statement.sql
@@ -9,3 +9,25 @@ CASE
 END AS 'AGE Bracket'
 FROM employee_demographics
 ;
+
+-- Pay Increase and Bonus 
+-- Less than 50k 5% increase 
+-- greater than 50k the 7% increase 
+-- Finance department 10 % Bonus 
+
+SELECT first_name, last_name, salary,
+CASE
+	WHEN salary < 50000 THEN salary * 1.05
+    When salary > 50000 Then salary * 1.07
+END AS New_salary,
+CASE
+	WHEN dept_id = 6 THEN salary * 0.10 
+END AS Bonus
+
+FROM employee_salary;
+
+SELECT * 
+FROM employee_salary;
+
+SELECT * 
+FROM parks_departments;
